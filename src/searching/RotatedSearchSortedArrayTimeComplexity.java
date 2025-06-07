e40f4d000b08a0b7f25c0a4650b1cd5eb032062e
Bhanu-28 <bhanupradeep123456@gmail.com>
2025-06-08 00:12:58 +0530

    rotatedSearchArray 100% 0ms

--- src/searching/RotatedSearchSortedArrayTimeComplexity.java
+++ src/searching/RotatedSearchSortedArrayTimeComplexity.java
@@ -3,52 +3,42 @@ package searching;
 public class RotatedSearchSortedArrayTimeComplexity {
     public static void main(String[] args) {
         int[] arr = {3,4,5,6,7,1,2};
-        System.out.println(search(arr,3));
+        int[] arr1 = {5,1,3};
+        int[] arr2 = {4,5,6,7,8,1,2,3};
+
+        System.out.println(search(arr2,8));
     }
 
-    static int search(int[] arr , int elementToSearch){
+    static int search(int[] nums , int target){
         int low = 0;
-        int high = arr.length-1;
+        int high = nums.length-1;
         int mid;
-        int indexofElement;
         while(low<=high){
             mid = low + (high-low)/2;
 
-            if(arr[mid]==elementToSearch){
+            if(nums[mid]==target){
                 return mid;
             }
-            else if(arr[mid]>=arr[low]){
+            else if(nums[mid]>=nums[low]){
              /* if array of mid is greater than or equal to low
               then that part of array low to mid is sorted. */
-                if(elementToSearch>=arr[low]&&elementToSearch<=arr[mid]){
-                    indexofElement = binarySearch(arr,low,mid,elementToSearch);
-                    if(indexofElement!=-1){
-                        return indexofElement;
+                if(target>=nums [low]&&target<=nums [mid]){
+                      high = mid - 1;
                     }
+                else{
+                    low = mid + 1;
                 }
-                    return binarySearch(arr,mid+1,high,elementToSearch);
             }
             else {
-//                elementToSearch>=arr[mid] && elementToSearch <=arr[high]
-                  return binarySearch(arr,mid+1,high,elementToSearch);
-            }
-        }
-        return low;
-    }
-
-    static int binarySearch(int[] arr, int low, int high, int target) {
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (arr[mid] == target) {
-                return mid;
-            } else if (arr[mid] > target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+                if(target>=nums [mid]&&target<=nums [high]){
+                    low = mid + 1;
+                }
+                else {
+                    high = mid -1;
+                }
             }
         }
         return -1;
     }
+    
 }
