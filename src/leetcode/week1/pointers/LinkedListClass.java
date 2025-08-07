b9c4e174b8d80ed7843e70245f4ea5424fd8c02f
Gorre Bhanu Pradeep Kumar <Gorre.Kumar@kpit.com>
2025-08-07 22:34:05 +0530

    Remove duplicates.

---
+++ src/leetcode/week1/pointers/LinkedListClass.java
@@ -0,0 +1,38 @@
+package leetcode.week1.pointers;
+
+import java.util.LinkedList;
+
+public class LinkedListClass {
+    public static void main(String[] args) {
+
+        LinkedList<String> cars = new LinkedList<>();
+
+
+        cars.add("JLR");
+        cars.add("Renault");
+
+
+        System.out.println("Linked list elements :"+cars);
+
+        cars.addFirst("BMW");
+
+        cars.addLast("Audi");
+
+        cars.remove("ford");
+
+
+        System.out.println("After adding few and removing few cars"+cars);
+
+
+
+        for(String car : cars){
+            System.out.println("car :" + car);
+        }
+
+        System.out.println();
+
+
+    }
+
+
+}
---
+++ src/leetcode/week1/pointers/RemoveDuplicates.java
@@ -0,0 +1,79 @@
+package leetcode.week1.pointers;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int[] values = {1,1,5,7,7,9,9,10,11,20,21,21,22};
+        Node head = createLinkedList(values);
+
+        printList(head);
+
+
+        head = removeDuplicates(head);
+
+        printList(head);
+
+    }
+
+    private static Node removeDuplicates(Node head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        Node current = head;
+        while(current!=null && current.next!=null){
+            //Duplicate found
+            if(current.data == current.next.data){
+                System.out.println("duplicate found :"+current.data+" "+ current.next.data);
+                current.next = current.next.next;
+            }
+            else{
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+
+    public static void printList(Node node){
+        while(node!=null){
+            System.out.println(node.data+" ");
+            node = node.next;
+        }
+        System.out.println();
+
+    }
+
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int x){
+            data = x;
+            next = null;
+        }
+    }
+
+
+
+    public static Node createLinkedList(int[] values){
+
+        Node head = null;
+        Node tail = null;
+        for(int value : values){
+            Node node = new Node(value);
+
+            if(head == null){
+                head = node;
+                tail = node;
+            }
+            else{
+                tail.next = node;
+                tail = node;
+            }
+        }
+        return head;
+    }
+}
