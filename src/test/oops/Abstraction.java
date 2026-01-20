32e6f9386df207e0d4f11b7d8b360bae4e6b9d1d
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-01-20 23:52:56 +0530

    Abstraction

---
+++ src/test/oops/Abstraction.java
@@ -0,0 +1,51 @@
+package test.oops;
+
+public class Abstraction {
+
+    public static void main(String[] args) {
+        TV remote = new TvRemote();
+
+        remote.turnOn();
+        remote.turnOff();
+
+
+        TV remote1 = new PlayRemote();
+        remote1.turnOn();
+        remote1.turnOff();
+
+    }
+
+}
+
+
+abstract class TV{
+    abstract void turnOn();
+
+    abstract void turnOff();
+}
+
+class TvRemote extends TV {
+    @Override
+    void turnOn() {
+        System.out.println("Tv is On");
+    }
+
+    @Override
+    void turnOff() {
+        System.out.println("Tv is off");
+    }
+}
+
+
+class PlayRemote extends TV {
+    @Override
+    void turnOn() {
+        System.out.println("Game is Started");
+    }
+
+    @Override
+    void turnOff() {
+        System.out.println("Game is Over");
+    }
+}
+
