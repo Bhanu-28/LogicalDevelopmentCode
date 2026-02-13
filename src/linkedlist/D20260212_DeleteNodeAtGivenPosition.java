be15249409919dee80d929994759ef245a5e8801
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-02-13 19:13:36 +0530

    handle position error edge cases

--- src/linkedlist/D20260212_DeleteNodeAtGivenPosition.java
+++ src/linkedlist/D20260212_DeleteNodeAtGivenPosition.java
@@ -30,16 +30,25 @@ public class D20260212_DeleteNodeAtGivenPosition {
 
         Node deleteANodeAtGivenPosition(int position){
 
-            Node temp = head;
-            if(position == 1 ){
-                head = temp.next;
+            if(position <=0 || head == null){
                 return head;
             }
 
+            if(position == 1 ){
+                head = head.next;
+                return head;
+            }
+
+            Node temp = head;
             Node prev = null;
-            for(int i = 1; i < position; i++){
+
+            for(int i = 1; i < position&& temp!=null; i++){
                 prev = temp;
                 temp = temp.next;
+            }
+
+            if(temp == null){
+                return head;
             }
 
             prev.next = temp.next;
@@ -49,7 +58,15 @@ public class D20260212_DeleteNodeAtGivenPosition {
 
         Node deleteANodeAtGivenPositionRecursive(Node head, int position){
 
-            if (position == 0){ return head.next; }
+            if(position < 1){
+                return head;
+            }
+
+            if(head == null){
+                return null;
+            }
+
+            if (position == 1){ return head.next; }
             head.next = deleteANodeAtGivenPositionRecursive(head.next, position-1);
             return head;
         }
