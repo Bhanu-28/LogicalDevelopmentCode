9473c7bfffdfa2dd1d008c69603cb0bb8b53a0ca
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-02-21 23:56:35 +0530

    string contains vowels

--- src/test/interview/day2/SwapNumbers.java
+++ src/test/interview/day2/SwapNumbers.java
@@ -8,9 +8,9 @@ public class SwapNumbers {
 
         System.out.println("a is " + a + " and b is " + b);
 
-        a = a + b;
-        b = a - b;
-        a = a - b;
+        a = a + b; // 10 + 20 = 30
+        b = a - b;  // 30 - 20 = 10
+        a = a - b;  // 30 - 10 = 20
 
         System.out.println("After swapping, a is " + a + " and b is " + b);
     }
