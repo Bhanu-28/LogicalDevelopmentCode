7e06551976e091f65965900aad18bdb9f0bffff2
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-05-20 10:30:00 +0530

    string malipulation

---
+++ src/test/java/test/interview/day9/StringMalipulationMap.java
@@ -0,0 +1,26 @@
+package test.interview.day9;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class StringMalipulationMap {
+	
+	public static void main(String[] args) {
+		
+
+	    List<String> input = Arrays.asList("apple","ball","cat","ant",null,"ANT");
+
+	    List<String> result = input.stream()
+	        .filter(Objects::nonNull)
+	        .filter(s -> s.toLowerCase().startsWith("a"))
+	        .map(String::toUpperCase)
+	        .distinct()
+	        .collect(Collectors.toList());
+
+	    System.out.println(result);
+
+	}
+
+}
