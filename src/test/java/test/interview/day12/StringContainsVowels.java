687e8271c580879e1a03a9926c5e060abab4003d
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-05-26 10:30:00 +0530

    string contains vowels

---
+++ src/test/java/test/interview/day12/StringContainsVowels.java
@@ -0,0 +1,17 @@
+package test.interview.day12;
+
+public class StringContainsVowels {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(stringContainsVowels("Hello")); // true
+		System.out.println(stringContainsVowels("TV")); // false
+	}
+	
+	
+	public static boolean stringContainsVowels(String input){
+		return input.toLowerCase().matches(".*[aeiou].*");
+	}
+
+}
