06a1ef7ed9023dd14e3556d8ee275113e8b1e8c8
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-05-27 10:30:00 +0530

    fibonacci series

--- src/test/java/test/interview/day12/StringContainsVowels.java
+++ src/test/java/test/interview/day12/StringContainsVowels.java
@@ -1,16 +1,14 @@
 package test.interview.day12;
 
 public class StringContainsVowels {
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println(stringContainsVowels("Hello")); // true
 		System.out.println(stringContainsVowels("TV")); // false
 	}
-	
-	
-	public static boolean stringContainsVowels(String input){
+
+	public static boolean stringContainsVowels(String input) {
 		return input.toLowerCase().matches(".*[aeiou].*");
 	}
 
