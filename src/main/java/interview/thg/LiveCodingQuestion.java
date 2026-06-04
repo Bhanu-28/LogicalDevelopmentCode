c03145686d3d574695eba4400ccaf5f173339081
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-06-04 10:30:00 +0530

    practice streams

--- src/main/java/interview/thg/LiveCodingQuestion.java
+++ src/main/java/interview/thg/LiveCodingQuestion.java
@@ -3,6 +3,7 @@ package interview.thg;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class LiveCodingQuestion {
 	
@@ -23,9 +24,53 @@ public class LiveCodingQuestion {
 			add(1); add(40); add(1034); add(50); add(1023); add(2); add(3152);
 		}};
 		 
-		Integer highest = integers.stream().max(Integer::compareTo).orElse(null);
+		Integer highest = integers.stream().max((x,y)-> x.compareTo(y)).orElse(null);
 		
-		System.out.println(highest);
+		Integer minimum = integers.stream().min(Integer::compareTo).orElse(null);
+
+		
+		System.out.println("highest : "+highest);
+		System.out.println("minimu : "+minimum);
+		
+		
+		
+		//practice
+		
+		String[][][] array1 = new String[][][]{{{"a", "b","c"}, {"c", "d","e"}, {"e", "f","g"}}};
+
+		// List<String> result = "a", "b", "c"....
+		
+		// Rule:Number of flatMaps = Number of dimensions − 1
+		
+		List<String> result1 = Arrays.stream(array1)
+							  .flatMap(arr -> Arrays.stream(arr))
+							  .flatMap(arr -> Arrays.stream(arr))
+							  .toList();
+		
+		System.out.println(result1);
+		
+		Integer[][] nums = {
+		        {1,2},
+		        {3,4}
+		};
+		
+		List<Integer> numsResult = Arrays.stream(nums)
+				  .flatMap(arr -> Arrays.stream(arr))
+				  .toList();
+		
+		Stream.of(nums)
+				  .flatMap(arr -> Arrays.stream(arr))
+				  .forEach(System.out::print);
+		
+		System.out.println(numsResult);
+		
+		
+		List<List<Integer>> numbers = Arrays.asList(
+	            Arrays.asList(1, 2),
+	            Arrays.asList(3, 4)
+	        );
+
+	       
 	}
 
 
