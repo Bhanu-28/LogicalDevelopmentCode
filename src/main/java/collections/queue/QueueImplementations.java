8d348e3ea9597ac49444eb55a125c6496087f453
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-06-30 17:26:41 +0530

    Queue Dequeue Stack Implementations

---
+++ src/main/java/collections/queue/QueueImplementations.java
@@ -0,0 +1,90 @@
+package collections.queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueImplementations {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		// Queue is interface implementation we can use LinkedList.
+		// FIFO
+		Queue<Integer> queue = new LinkedList<>();
+		
+		queue.add(1);
+		queue.offer(2);
+		queue.offer(3);
+		
+		
+		System.out.println("-----Queue start-----");
+		
+		System.out.println(queue);
+		
+		System.out.println("peek :"+queue.peek());
+		System.out.println(queue.poll());
+		System.out.println("peek :"+queue.peek());
+		System.out.println(queue.poll());
+		System.out.println("peek :"+queue.peek());
+		System.out.println(queue.poll());
+		
+		// No element present it will return null
+		System.out.println("peek :"+queue.peek());
+		
+		System.out.println("-----Queue END-----");
+		
+		
+		// For stack there is a separate class which is present. use this.
+		System.out.println("-----Stack start-----");
+		Stack<Integer> stack = new Stack<>();
+		
+		stack.push(1);
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		
+		System.out.println(stack);
+		System.out.println(stack.pop());
+		System.out.println("-----Stack end-----");
+		
+		
+		System.out.println("-----DeQueue start-----");
+		Deque<Integer> deque1 = new ArrayDeque<>();
+		
+		// Using Deque as a Queue (FIFO)
+		deque1.offerLast(1);
+		deque1.offerLast(2);
+		deque1.offerLast(3);
+		
+		System.out.println(deque1);
+		
+		System.out.println(deque1.pollFirst());
+		System.out.println(deque1.peekFirst());
+		
+		System.out.println(deque1);
+		
+		Deque<Integer> deque2 = new ArrayDeque<>();
+		
+		// Using Deque as a Stack (LIFO)
+		
+		deque2.offerFirst(1);
+		deque2.offerFirst(2);
+		deque2.offerFirst(3);
+		
+		System.out.println(deque2);
+		
+		System.out.println(deque2.pollFirst());
+		
+		
+		System.out.println(deque2);
+		
+		System.out.println("-----DeQueue END-----");
+		
+	}
+	
+
+}
