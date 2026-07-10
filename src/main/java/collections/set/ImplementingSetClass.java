3528d14c6376d55ec4ab08085072cc4140f9783d
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-07-10 10:30:00 +0530

    Implementing set class

---
+++ src/main/java/collections/set/ImplementingSetClass.java
@@ -0,0 +1,51 @@
+package collections.set;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ImplementingSetClass {
+	
+	public static void main(String[] args) {
+		
+		
+		List<Integer> arrayList = new ArrayList<>();
+		
+		
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.add(3);
+		arrayList.add(5);
+		arrayList.add(8);
+		
+		Set<Integer> set1 = new HashSet<>(arrayList);
+		
+		System.out.println("set 1 : "+set1);
+		
+		Set<Integer> set2 = new HashSet<>();
+		
+		set2.add(8);
+		set2.add(1);
+		set2.add(2);
+		
+		System.out.println("set 2 :"+set2);
+		
+		System.out.println("after retaining");
+		
+		
+		/*
+		 * 
+		 * set 2 will remain unchanged 
+		 * All the changes will be done on set 1 
+		 * Now set1 will have all the intersection elements.
+		 */
+		set1.retainAll(set2);
+		
+		System.out.println("set 1 : "+set1);
+		System.out.println("set 2 : "+set2);
+		
+		
+	}
+
+}
