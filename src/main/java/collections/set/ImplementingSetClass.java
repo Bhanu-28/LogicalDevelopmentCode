24c35cb8136eee5a790066782da5110c0fb8831c
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-07-12 10:30:00 +0530

    Linked hashset which maintains order

--- src/main/java/collections/set/ImplementingSetClass.java
+++ src/main/java/collections/set/ImplementingSetClass.java
@@ -2,6 +2,7 @@ package collections.set;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -42,6 +43,7 @@ public class ImplementingSetClass {
 		 * All the changes will be done on set 1 
 		 * Now set1 will have all the intersection elements.
 		 */
+		// Intersection
 		set1.retainAll(set2);
 		
 		
@@ -51,10 +53,35 @@ public class ImplementingSetClass {
 		// common will be removed from set 1 so will be blank as both were same.
 		System.out.println("After removing");
 		
+		// difference
 		set1.removeAll(set2);
 		
 		System.out.println("set 1 : "+set1);
 		System.out.println("set 2 : "+set2);
+		
+		
+		
+		
+		Set<Integer> set3 = new HashSet<>();
+		
+		set3.add(10);
+		set3.add(20);
+		set3.add(30);
+		
+		//union
+		set3.addAll(set2);
+		System.out.println("set 3 : "+set3);
+		System.out.println("set 2 : "+set2);
+		
+		
+		Set<Integer> set4 = new LinkedHashSet<>();
+		
+		set4.add(10);
+		set4.add(20);
+		set4.add(30);
+		
+		System.out.println("LinkedHashSet which maintains order: "+set4);
+		
 		
 		
 	}
