eef5b9ae36f3b686e315ebc3e99b76eb2be6bc71
Bhanu-28 <bhanupradeep123456@gmail.com>
2026-07-15 11:01:21 +0530

    Adjacecny map entry set and keyset

--- src/main/java/collections/map/ImplementingMapClass.java
+++ src/main/java/collections/map/ImplementingMapClass.java
@@ -1,7 +1,11 @@
 package collections.map;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class ImplementingMapClass {
 	
@@ -28,6 +32,48 @@ public class ImplementingMapClass {
 		
 		System.out.println(studentMap.get(1));
 		System.out.println(studentMap.containsValue(new StudentMarks(20, 40)));
+		
+		
+		/*
+		 * Creating an adjacency list for graph questions using HashMap
+		 *  
+		 *  1 - 3
+		 *  3 - 5
+		 *  1 - 2
+		 */
+		
+		
+		Map<Integer , List<Integer>> adjacencyMap = new HashMap<>();
+		
+//		if(adjacencyMap.get(1)==null) {
+//			adjacencyMap.put(1, new ArrayList<>());
+//		}
+//		
+//		adjacencyMap.get(1).add(3);
+		
+		// can rewrite above piece of code using computeIfAbsent
+		
+		adjacencyMap.computeIfAbsent(1, function-> new ArrayList<>()).add(3);
+		adjacencyMap.computeIfAbsent(1, function-> new ArrayList<>()).add(2);
+		adjacencyMap.computeIfAbsent(3, function-> new ArrayList<>()).add(5);
+		
+		
+		Set<Entry<Integer, String>> entrySet = map.entrySet();
+		
+		for(Map.Entry<Integer,String> entry : entrySet) {
+			
+			System.out.println("key :"+entry.getKey()+" Value : "+entry.getValue());
+			
+		}
+		
+		Set<Integer> keySet = map.keySet();
+		System.out.println("Keys ");
+		for(Integer key : keySet) {
+			System.out.print(key+",");
+		}
+		
+		
+		
 		
 		
 		
